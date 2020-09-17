0de20066666ea028fcd5464d6fd77db396018085
Danilo Malafaia <danilofmleite@gmail.com>
2020-09-16 22:28:34 -0300

    Draw minimal bubble in the middle of canvas

--- lib/bubbles.dart
+++ lib/bubbles.dart
@@ -2,10 +2,35 @@ library bubbles;
 
 import 'dart:ui';
 
+import 'package:flutter/widgets.dart';
+
 class Bubble {
   Bubble(this._color, this._radius);
 
   final Color _color;
   final double _radius;
+
+  void draw(Canvas canvas, Size canvasSize) {
+    final paint = Paint()
+      ..color = _color
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.fill;
+
+    canvas.drawCircle(
+        Offset(canvasSize.width / 2, canvasSize.height / 2), _radius, paint);
+  }
 }
 
+class BubblePainter extends CustomPainter {
+  const BubblePainter(this._bubble);
+
+  final Bubble _bubble;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _bubble.draw(canvas, size);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
